fd07e699292f6949f9cee2a869c2870c11f1fbcd
elpe2005 <111962532+elpe2005@users.noreply.github.com>
2023-02-17 11:13:54 +0100

    Update Program.cs

--- Testuppgift2Elliot/Program.cs
+++ Testuppgift2Elliot/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Här kan du omvandla från celsius till fahrenheit eller tvärtom");
+            Console.WriteLine(CtillF);
+            Console.WriteLine("Välj mellan 1 och 2.");
+        }
+        static int CtillF(int Celsius)
+        {
+            int F = ((32 + Celsius * 1.8));
+        }
+        static int tal(int Celsius,int F)
+        {
+            switch (tal)
+            {
+                case 1:
+                    Console.WriteLine("Skriv två tal här nere så omvandlar du celsius till fahrenheit");
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Skriv två tal här nere så omvandlar du Fahrenheit till Celsius");
+                    Console.ReadLine();
+                    break ;
+            }
+        }
+    }
+}
